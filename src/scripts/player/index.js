93302f89255233bb901e476630ad343a7673a4f3
Yago Lopes <yagolopeslazaro@gmail.com>
2021-01-30 11:26:29 -0300

    tentativa de colisao

--- src/scripts/player/index.js
+++ src/scripts/player/index.js
@@ -12,33 +12,81 @@ function mount() {
   player.style.position = "relative";
   const board = document.getElementById("board");
   board.appendChild(player);
-  move(player);
+  checkAndMove(player);
 }
 
-function move(player) {
+function checkAndMove(player) {
   document.addEventListener("keydown", (event) => {
     const keyName = event.key;
-    if (keyName === "ArrowDown") {
-      boxTop += 10;
-      player.style.top = `${boxTop}px`;
-      player.style.transform = "rotate(100deg)";
-    }
-    if (keyName === "ArrowUp") {
-      boxTop -= 10;
-      player.style.top = `${boxTop}px`;
-      player.style.transform = "rotate(-85deg)";
-    }
-    if (keyName === "ArrowRight") {
-      boxLeft += 15;
-      player.style.left = `${boxLeft}px`;
-      player.style.transform = "rotate(0deg)";
-    }
-    if (keyName === "ArrowLeft") {
-      boxLeft -= 15;
-      player.style.left = `${boxLeft}px`;
-      player.style.transform = "rotate(-175deg)";
-    }
+    moveAndCheckColision(keyName, player);
   });
+}
+
+function moveAndCheckColision(keyName, player) {
+  const blocks = document.querySelectorAll(".block");
+
+  switch (keyName) {
+    case "ArrowDown":
+      const arrowDown = [...blocks].find((elemento) => {
+        if (elemento.offsetRight === player.offsetRight) {
+          return elemento;
+        }
+        return null;
+      });
+
+      if (arrowDown) {
+        boxTop += 10;
+        player.style.top = `${boxTop}px`;
+        player.style.transform = "rotate(100deg)";
+      }
+      return;
+
+    case "ArrowUp":
+      const arrowUp = [...blocks].find((elemento) => {
+        if (elemento.offsetRight === player.offsetRight) {
+          return elemento;
+        }
+        return null;
+      });
+      if (arrowUp) {
+        boxTop -= 10;
+        player.style.top = `${boxTop}px`;
+        player.style.transform = "rotate(-85deg)";
+      }
+      return;
+
+    case "ArrowRight":
+      const arrowRight = [...blocks].find((elemento) => {
+        if (elemento.offsetRight === player.offsetRight) {
+          return elemento;
+        }
+        return null;
+      });
+      if (!arrowRight) {
+        boxLeft += 15;
+        player.style.left = `${boxLeft}px`;
+        player.style.transform = "rotate(0deg)";
+      }
+      return;
+
+    case "ArrowLeft":
+      const arrowLeft = [...blocks].find((elemento) => {
+        if (elemento.offsetLeft === player.offsetLeft) {
+          return elemento;
+        }
+        return null;
+      });
+      if (!arrowLeft) {
+        boxLeft -= 15;
+        player.style.left = `${boxLeft}px`;
+        player.style.transform = "rotate(-175deg)";
+      }
+
+      return;
+
+    default:
+      return;
+  }
 }
 
 start();
